36afd6ec1add06ce372bd4fd4bf8cc49339fa2c5
Rodolfo Cortese <23123652+rcortese@users.noreply.github.com>
2025-12-07 22:09:05 -0300

    Align env sync with instance-only compose discovery

--- scripts/check_health.sh
+++ scripts/check_health.sh
@@ -320,31 +320,8 @@ COMPOSE_FILES="$COMPOSE_ROOT_FILE"
 
 mapfile -t LOG_TARGETS < <(env_file_chain__parse_list "${HEALTH_SERVICES:-}") || true
 
-declare -a METADATA_APP_NAMES=()
-declare -a METADATA_PRIMARY_APPS=()
-if [[ -n "$INSTANCE_NAME" ]]; then
-  if load_compose_instances "$REPO_ROOT"; then
-    if [[ -n "${COMPOSE_INSTANCE_APP_NAMES[$INSTANCE_NAME]:-}" ]]; then
-      mapfile -t METADATA_APP_NAMES < <(printf '%s\n' "${COMPOSE_INSTANCE_APP_NAMES[$INSTANCE_NAME]}")
-      if [[ ${#METADATA_APP_NAMES[@]} -gt 0 ]]; then
-        __metadata_app_name=""
-        for __metadata_app_name in "${METADATA_APP_NAMES[@]}"; do
-          if [[ -n "${COMPOSE_APP_BASE_FILES[$__metadata_app_name]:-}" ]]; then
-            METADATA_PRIMARY_APPS+=("$__metadata_app_name")
-          fi
-        done
-        unset __metadata_app_name
-      fi
-    fi
-  fi
-fi
-
 if [[ ${#LOG_TARGETS[@]} -eq 0 ]]; then
-  if [[ "$CHANGED_TO_REPO_ROOT" == false && ${#METADATA_PRIMARY_APPS[@]} -gt 0 ]]; then
-    LOG_TARGETS=("${METADATA_PRIMARY_APPS[@]}")
-  elif [[ ${#METADATA_APP_NAMES[@]} -gt 0 ]]; then
-    LOG_TARGETS=("${METADATA_APP_NAMES[@]}")
-  fi
+  :
 fi
 
 primary_targets=("${LOG_TARGETS[@]}")
